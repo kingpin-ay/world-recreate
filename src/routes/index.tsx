5faf5311e12a6663549b6ce5137a1946cc45271c
kingpin-ay <ayushmondal001@gmail.com>
2026-01-04 12:26:21 +0530

    feat: add initial root route with basic "World Recreate" UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute('/')({ component: App })
 function App() {
   return (
     <div className="h-full w-full flex justify-center items-center pt-1 pb-1">
-      <h1 className="text-2xl font-bold">World Recreate</h1>
+      <h1 className="text-2xl font-bold text-white">World Recreate</h1>
     </div>
   )
 }
